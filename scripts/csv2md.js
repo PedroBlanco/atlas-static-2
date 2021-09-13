70c28efb2659e3bc1bea8e27ce68997f3f128d59
Pedro Blanco Wasmer <9304344+PedroBlanco@users.noreply.github.com>
2021-09-13 15:17:00 +0200

    feat: Implemento CSV -> MarkDown -> HTML
    Se realiza en 2 pasos:
    
    1. ```yarn run csv2md```
      Parseamos el archivo CSV y creamos los archivos MD en el directorio
      src/mapas
    2. ```bakeit serve```
      Puede estar ejecutándose en otro terminal, pues conforme detecta
      cambios los procesa y crea HTML a partir de los MD

--- scripts/csv2md.js
+++ scripts/csv2md.js
@@ -1,8 +1,29 @@
 const fs = require('fs');
 const Papa = require('papaparse');
-var filename = "test1.csv";
+const Handlebars = require('handlebars');
 
-console.log( "*** Empezando procesado de " + filename + " ***" );
+// Datos CSV de origen
+var filename = "test1.csv";
+// var srcdir_mapas = "csv_src";
+
+// Directorio destino de los archivos MarkDown
+var destdir_mapas = "src/mapas";
+
+// Plantilla de salida de MarkDown
+var md_template_filename = "src/.theme/map.md.hbs";
+var md_template = '';
+
+console.log( "*** Empezando procesado de " + filename + " con destino "+ destdir_mapas + " ***" );
+
+// Leemos la plantilla de los MarkDown
+fs.readFile(md_template_filename, 'utf8' , (err, data) => {
+    if (err) {
+      console.error(err)
+      return
+    }
+    console.log ( data );
+    md_template = Handlebars.compile(data);
+});
 
 fs.readFile(filename, 'utf8' , (err, data) => {
     if (err) {
@@ -10,13 +31,26 @@ fs.readFile(filename, 'utf8' , (err, data) => {
       return
     }
     Papa.parse(data, {
+        header: true,
         step: function(row) {
-            console.log ( row.data );
+            // console.log ( row.data ); // Mostramos los datos en bruto de la línea
+
+            // Leemos cada línea y la convertimos de JSON a MarkDown
+            var result = md_template( row.data );
+            // console.log ( result ); // Mostramos la línea convertida a MarkDown
+
+            // Creamos un archivo en destdir_mapas por cada línea convertida a MarkDown
+            fs.writeFile( destdir_mapas + '/' + row.data['Nombre del mapa'] + '.md', result, err => {
+                if (err) {
+                  console.error(err)
+                  return
+                }
+                //file written successfully
+                console.log ( '+ Creando ' + destdir_mapas + '/' + row.data['Nombre del mapa'] + '.md' );
+              });
         },
         complete: function() {
-            console.log( "*** Terminando procesado de " + filename + " ***" );
+            console.log( "*** Terminando procesado de " + filename + " con destino "+ destdir_mapas + " ***" );
         }
     });
-  });
-
-
+});
