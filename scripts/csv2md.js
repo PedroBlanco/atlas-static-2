950cc6fb91436fe717e42a1d93c64b8379ed5144
Pedro Blanco Wasmer <9304344+PedroBlanco@users.noreply.github.com>
2022-09-21 15:10:07 +0200

    feat: Inicio implementación de listado de autores

--- scripts/csv2md.js
+++ scripts/csv2md.js
@@ -8,21 +8,38 @@ var filename = "test5.csv";
 
 // Directorio destino de los archivos MarkDown
 var destdir_mapas = "src/mapas";
+var destdir_autores = "src/autores";
 
 // Plantilla de salida de MarkDown
 var md_template_filename = "src/.theme/map.md.hbs";
 var md_template = '';
+var md_template_autor_filename = "src/.theme/author.md.hbs";
+var md_template_autor = '';
+const autores = [];
 
 console.log( "*** Empezando procesado de " + filename + " con destino "+ destdir_mapas + " ***" );
 
-// Leemos la plantilla de los MarkDown
+// Leemos las plantillas de los MarkDown
+// Mapas
 fs.readFile(md_template_filename, 'utf8' , (err, data) => {
-    if (err) {
-      console.error(err)
-      return
-    }
-    console.log ( data );
-    md_template = Handlebars.compile(data);
+  if (err) {
+    console.error(err)
+    return
+  }
+  console.log ( 'Plantilla de mapas:' );
+  console.log ( data );
+  md_template = Handlebars.compile(data);
+});
+
+// Autores
+fs.readFile(md_template_autor_filename, 'utf8' , (err, data) => {
+  if (err) {
+    console.error(err)
+    return
+  }
+  console.log ( 'Plantilla de autores:' );
+  console.log ( data );
+  md_template_autor = Handlebars.compile(data);
 });
 
 fs.readFile(filename, 'utf8' , (err, data) => {
@@ -37,14 +54,18 @@ fs.readFile(filename, 'utf8' , (err, data) => {
             // console.log ( row.data ); // Mostramos los datos en bruto de la línea
 
             // Leemos cada línea y la convertimos de JSON a MarkDown
-            var result = md_template( row.data );
+            let result = md_template( row.data );
             // console.log ( result ); // Mostramos la línea convertida a MarkDown
 
+            // Nombre del autor y nombre de su archivo
+            let _autor = row.data['autor_nombre'] + ' ' + row.data['autor_apellidos'];
+            let _autor_filename = destdir_autores + '/' + _autor.normalize('NFD').replace(/[\u0300-\u036f]/g,"").replace(/[\/\s\:\.\,]/g,'_') + '.md';
+
             // Creamos un archivo en destdir_mapas por cada línea convertida a MarkDown
-            console.log ( 'Nombre del mapa: ' + row.data['Nombre del mapa'] );
+            console.log ( 'Nombre del mapa: ' + row.data['Nombre del mapa'] + ' / Autor/a: ' + _autor );
 
             // Normalizamos el nombre del mapa y quitamos los diacríticos (aunque queda la ñ)
-            var _map_filename = destdir_mapas + '/' + row.data['Nombre del mapa'].normalize('NFD').replace(/[\u0300-\u036f]/g,"").replace(/[\/\s\:\.\,]/g,'_') + '.md'
+            let _map_filename = destdir_mapas + '/' + row.data['Nombre del mapa'].normalize('NFD').replace(/[\u0300-\u036f]/g,"").replace(/[\/\s\:\.\,]/g,'_') + '.md';
             
             fs.writeFile( _map_filename, result, err => {
                 if (err) {
@@ -53,10 +74,23 @@ fs.readFile(filename, 'utf8' , (err, data) => {
                 }
                 //file written successfully
                 console.log ( '+ Creando ' + _map_filename );
+                let mapa_actual = { 'name': row.data['Nombre del mapa'], 'filename': _map_filename }
+                let autor_actual = autores.find( autor_actual => autor_actual.name === _autor )
+                if ( autor_actual != null ) {
+                  autor_actual.mapas.push ( mapa_actual )
+                } else {
+                  autores.push ({
+                    name: _autor,
+                    filename: _autor_filename,
+                    mapas: [mapa_actual]
+                  })
+                }
               });
         },
         complete: function() {
             console.log( "*** Terminando procesado de " + filename + " con destino "+ destdir_mapas + " ***" );
         }
     });
+    console.log('***************');
+    console.log(autores);
 });
---
+++ src/autores/index.md
@@ -0,0 +1,11 @@
+---
+template: autores
+title: Autores
+search: true
+cluster: true
+back_link: false
+latlng: 37.36796,-4.564819
+corner_NW: 38.728362,-7.547607
+corner_SE: 35.982437,-1.582031
+mapzoom: 8
+---
