fc5654b4da134e5736ec1f7f6146837f1eb7b0ca
Pedro Blanco Wasmer <9304344+PedroBlanco@users.noreply.github.com>
2022-09-27 17:17:04 +0200

    feat: Creo las plantillas de autores a la vez que las de los mapas
    Todavía faltan procesar los mapas de cada autor.

--- scripts/csv2md.js
+++ scripts/csv2md.js
@@ -21,6 +21,7 @@ console.log( "*** Empezando procesado de " + filename + " con destino "+ destdir
 
 // Leemos las plantillas de los MarkDown
 // Mapas
+/*
 fs.readFile(md_template_filename, 'utf8' , (err, data) => {
   if (err) {
     console.error(err)
@@ -30,8 +31,22 @@ fs.readFile(md_template_filename, 'utf8' , (err, data) => {
   console.log ( data );
   md_template = Handlebars.compile(data);
 });
+*/
+try {
+  var data = fs.readFileSync(md_template_filename, 'utf8');
+  console.log ( 'Plantilla de mapas:' );
+  console.log ( data );
+  md_template = Handlebars.compile(data);
+} catch (err) {
+  if (err) {
+    console.error(err)
+    return
+  }
+}
+
 
 // Autores
+/*
 fs.readFile(md_template_autor_filename, 'utf8' , (err, data) => {
   if (err) {
     console.error(err)
@@ -41,59 +56,162 @@ fs.readFile(md_template_autor_filename, 'utf8' , (err, data) => {
   console.log ( data );
   md_template_autor = Handlebars.compile(data);
 });
+*/
+try {
+  var data = fs.readFileSync(md_template_autor_filename, 'utf8');
+  console.log ( 'Plantilla de autores:' );
+  console.log ( data );
+  md_template_autor = Handlebars.compile(data);
+} catch (err) {
+  if (err) {
+    console.error(err)
+    return
+  }
+}
 
+/*
 fs.readFile(filename, 'utf8' , (err, data) => {
-    if (err) {
-      console.error(err)
-      return
-    }
-    Papa.parse(data, {
-        header: true,
-        skipEmptyLines: true,
-        step: function(row) {
-            // console.log ( row.data ); // Mostramos los datos en bruto de la línea
+  if (err) {
+    console.error(err)
+    return
+  }
+  Papa.parse(data, {
+      header: true,
+      skipEmptyLines: true,
+      step: function(row) {
+        console.group ();
+        // console.log ( row.data ); // Mostramos los datos en bruto de la línea
 
-            // Leemos cada línea y la convertimos de JSON a MarkDown
-            let result = md_template( row.data );
-            // console.log ( result ); // Mostramos la línea convertida a MarkDown
+        // Leemos cada línea y la convertimos de JSON a MarkDown
+        let result = md_template( row.data );
+        // console.log ( result ); // Mostramos la línea convertida a MarkDown
 
-            // Nombre del autor y nombre de su archivo
-            let _autor = row.data['autor_nombre'] + ' ' + row.data['autor_apellidos'];
-            let _autor_filename = destdir_autores + '/' + _autor.normalize('NFD').replace(/[\u0300-\u036f]/g,"").replace(/[\/\s\:\.\,]/g,'_') + '.md';
+        // Nombre del autor y nombre de su archivo
+        let _autor = row.data['autor_nombre'] + ' ' + row.data['autor_apellidos'];
+        let _autor_filename = destdir_autores + '/' + _autor.normalize('NFD').replace(/[\u0300-\u036f]/g,"").replace(/[\/\s\:\.\,]/g,'_') + '.md';
 
-            // Creamos un archivo en destdir_mapas por cada línea convertida a MarkDown
-            console.log ( 'Nombre del mapa: ' + row.data['Nombre del mapa'] + ' / Autor/a: ' + _autor );
+        // Creamos un archivo en destdir_mapas por cada línea convertida a MarkDown
+        console.log ( 'Nombre del mapa: ' + row.data['Nombre del mapa'] + ' / Autor/a: ' + _autor );
 
-            // Normalizamos el nombre del mapa y quitamos los diacríticos (aunque queda la ñ)
-            let _map_filename = destdir_mapas + '/' + row.data['Nombre del mapa'].normalize('NFD').replace(/[\u0300-\u036f]/g,"").replace(/[\/\s\:\.\,]/g,'_') + '.md';
-            
-            fs.writeFile( _map_filename, result, err => {
-                if (err) {
-                  console.error(err)
-                  return
-                }
-                //file written successfully
-                // console.log ( '+ Creando ' + _map_filename + ' de ' + _autor );
-                console.log ( '+ Creando ' + _map_filename );
-                let mapa_actual = { 'name': row.data['Nombre del mapa'], 'filename': _map_filename }
-                // FIXME: fallo aquí
-                let autor_actual = autores.find( autor_actual => autor_actual.name === _autor )
-                console.log ( '+ Autor: ' + autor_actual )
-                if ( autor_actual != null ) {
-                  autor_actual.mapas.push ( mapa_actual )
-                } else {
-                  autores.push ({
-                    name: _autor,
-                    filename: _autor_filename,
-                    mapas: [mapa_actual]
-                  })
-                }
-              });
-        },
-        complete: function() {
-            console.log( "*** Terminando procesado de " + filename + " con destino "+ destdir_mapas + " ***" );
+        // Normalizamos el nombre del mapa y quitamos los diacríticos (aunque queda la ñ)
+        let _map_filename = destdir_mapas + '/' + row.data['Nombre del mapa'].normalize('NFD').replace(/[\u0300-\u036f]/g,"").replace(/[\/\s\:\.\,]/g,'_') + '.md';
+         
+        // fs.writeFile( _map_filename, result, err => {
+        //     if (err) {
+        //       console.error(err)
+        //       return
+        //     }
+        //     //file written successfully
+        //     // console.log ( '+ Creando ' + _map_filename + ' de ' + _autor );
+        //     let mapa_actual = { 'name': row.data['Nombre del mapa'], 'filename': _map_filename }
+        //     console.log ( '+ Creando ' + _map_filename + '->' + JSON.stringify(mapa_actual) );
+        //     // FIXME: fallo aquí
+        //     let autor_actual = autores.find( autor_actual => autor_actual.name === _autor )
+        //     if ( autor_actual != null ) {
+        //       autor_actual.mapas.push ( mapa_actual )
+        //       console.log ( '\x1b[36m%s\x1b[0m', '+ Autor existente: ' + autor_actual.name )
+        //     } else {
+        //       let _autor_nuevo = {
+        //         name: _autor,
+        //         filename: _autor_filename,
+        //         mapas: [mapa_actual]
+        //       }
+        //       autores.push ( _autor_nuevo );
+        //       console.log ( '\x1b[36m%s\x1b[0m', '+ Autor nuevo: ' + JSON.stringify(_autor_nuevo ) );
+        //     }
+        //   });
+
+          fs.writeFileSync( _map_filename, result );
+
+          // console.log ( '+ Creando ' + _map_filename + ' de ' + _autor );
+          let mapa_actual = { 'name': row.data['Nombre del mapa'], 'filename': _map_filename }
+          console.log ( '+ Creando ' + _map_filename + '->' + JSON.stringify(mapa_actual) );
+          // FIXME: fallo aquí
+          let autor_actual = autores.find( autor_actual => autor_actual.name === _autor )
+          if ( autor_actual != null ) {
+            autor_actual.mapas.push ( mapa_actual )
+            console.log ( '\x1b[36m%s\x1b[0m', '+ Autor existente: ' + autor_actual.name )
+          } else {
+            let _autor_nuevo = {
+              name: _autor,
+              filename: _autor_filename,
+              mapas: [mapa_actual]
+            }
+            autores.push ( _autor_nuevo );
+            console.log ( '\x1b[36m%s\x1b[0m', '+ Autor nuevo: ' + JSON.stringify(_autor_nuevo ) );
+          }
+
+        console.groupEnd ();
+      },
+      complete: function() {
+          console.log( "*** Terminando procesado de " + filename + " con destino "+ destdir_mapas + " ***" );
+          autores.forEach ( autor => {
+                          
+          });
+          console.log ( "Autores: " + JSON.stringify ( autores ) );
         }
-    });
-    console.log('***************');
-    console.log(autores);
+  });
 });
+*/
+
+try {
+  var data = fs.readFileSync(filename, 'utf8');
+  Papa.parse(data, {
+    header: true,
+    skipEmptyLines: true,
+    step: function(row) {
+      console.group ();
+      // console.log ( row.data ); // Mostramos los datos en bruto de la línea
+
+      // Leemos cada línea y la convertimos de JSON a MarkDown
+      let result = md_template( row.data );
+      // console.log ( result ); // Mostramos la línea convertida a MarkDown
+
+      // Nombre del autor y nombre de su archivo
+      let _autor = row.data['autor_nombre'] + ' ' + row.data['autor_apellidos'];
+      let _autor_filename = destdir_autores + '/' + _autor.normalize('NFD').replace(/[\u0300-\u036f]/g,"").replace(/[\/\s\:\.\,]/g,'_') + '.md';
+
+      // Creamos un archivo en destdir_mapas por cada línea convertida a MarkDown
+      console.log ( 'Nombre del mapa: ' + row.data['Nombre del mapa'] + ' / Autor/a: ' + _autor );
+
+      // Normalizamos el nombre del mapa y quitamos los diacríticos (aunque queda la ñ)
+      let _map_filename = destdir_mapas + '/' + row.data['Nombre del mapa'].normalize('NFD').replace(/[\u0300-\u036f]/g,"").replace(/[\/\s\:\.\,]/g,'_') + '.md';
+       
+      fs.writeFileSync( _map_filename, result );
+
+      // console.log ( '+ Creando ' + _map_filename + ' de ' + _autor );
+      let mapa_actual = { 'name': row.data['Nombre del mapa'], 'filename': _map_filename }
+      console.log ( '+ Creando ' + _map_filename + '->' + JSON.stringify(mapa_actual) );
+      // FIXME: fallo aquí
+      let autor_actual = autores.find( autor_actual => autor_actual.name === _autor )
+      if ( autor_actual != null ) {
+        autor_actual.mapas.push ( mapa_actual )
+        console.log ( '\x1b[36m%s\x1b[0m', '+ Autor existente: ' + autor_actual.name )
+      } else {
+        let _autor_nuevo = {
+          name: _autor,
+          filename: _autor_filename,
+          mapas: [mapa_actual]
+        }
+        autores.push ( _autor_nuevo );
+        console.log ( '\x1b[36m%s\x1b[0m', '+ Autor nuevo: ' + JSON.stringify(_autor_nuevo ) );
+      }
+
+      console.groupEnd ();
+    },
+    complete: function() {
+        console.log( "*** Terminando procesado de " + filename + " con destino "+ destdir_mapas + " ***" );
+        console.log ( "Autores: " + JSON.stringify ( autores ) );
+        autores.forEach ( autor => {
+          let result = md_template_autor ( autor );
+          fs.writeFileSync( autor.filename, result );
+        });
+    }
+  });
+} catch (err) {
+  if (err) {
+    console.error(err)
+    return
+  }
+}
+
