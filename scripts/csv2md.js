5f5ef9eb5e49e19a222786e7eebba4fac12fcb59
Pedro Blanco Wasmer <9304344+PedroBlanco@users.noreply.github.com>
2022-09-27 19:38:57 +0200

    feat: Genero lista de autores y cada autor

--- scripts/csv2md.js
+++ scripts/csv2md.js
@@ -10,6 +10,10 @@ var filename = "test5.csv";
 var destdir_mapas = "src/mapas";
 var destdir_autores = "src/autores";
 
+// Directorio destino de los archivos MarkDown
+var destlink_mapas = "mapas";
+var destlink_autores = "autores";
+
 // Plantilla de salida de MarkDown
 var md_template_filename = "src/.theme/map.md.hbs";
 var md_template = '';
@@ -169,18 +173,18 @@ try {
 
       // Nombre del autor y nombre de su archivo
       let _autor = row.data['autor_nombre'] + ' ' + row.data['autor_apellidos'];
-      let _autor_filename = destdir_autores + '/' + _autor.normalize('NFD').replace(/[\u0300-\u036f]/g,"").replace(/[\/\s\:\.\,]/g,'_') + '.md';
+      let _autor_name = _autor.normalize('NFD').replace(/[\u0300-\u036f]/g,"").replace(/[\/\s\:\.\,]/g,'_');
 
       // Creamos un archivo en destdir_mapas por cada línea convertida a MarkDown
       console.log ( 'Nombre del mapa: ' + row.data['Nombre del mapa'] + ' / Autor/a: ' + _autor );
 
       // Normalizamos el nombre del mapa y quitamos los diacríticos (aunque queda la ñ)
-      let _map_filename = destdir_mapas + '/' + row.data['Nombre del mapa'].normalize('NFD').replace(/[\u0300-\u036f]/g,"").replace(/[\/\s\:\.\,]/g,'_') + '.md';
+      let _map_filename = row.data['Nombre del mapa'].normalize('NFD').replace(/[\u0300-\u036f]/g,"").replace(/[\/\s\:\.\,]/g,'_');
        
-      fs.writeFileSync( _map_filename, result );
+      fs.writeFileSync( destdir_mapas + '/' + _map_filename + '.md' , result );
 
       // console.log ( '+ Creando ' + _map_filename + ' de ' + _autor );
-      let mapa_actual = { 'name': row.data['Nombre del mapa'], 'filename': _map_filename }
+      let mapa_actual = { 'name': row.data['Nombre del mapa'], 'filename': 'mapas/' + _map_filename +'/' }
       console.log ( '+ Creando ' + _map_filename + '->' + JSON.stringify(mapa_actual) );
       // FIXME: fallo aquí
       let autor_actual = autores.find( autor_actual => autor_actual.name === _autor )
@@ -190,11 +194,11 @@ try {
       } else {
         let _autor_nuevo = {
           name: _autor,
-          filename: _autor_filename,
+          filename: _autor_name,
           mapas: [mapa_actual]
         }
         autores.push ( _autor_nuevo );
-        console.log ( '\x1b[36m%s\x1b[0m', '+ Autor nuevo: ' + JSON.stringify(_autor_nuevo ) );
+        console.log ( '\x1b[36m%s\x1b[0m', '+ Autor nuevo: ' + JSON.stringify ( _autor_nuevo ) );
       }
 
       console.groupEnd ();
@@ -203,8 +207,10 @@ try {
         console.log( "*** Terminando procesado de " + filename + " con destino "+ destdir_mapas + " ***" );
         console.log ( "Autores: " + JSON.stringify ( autores ) );
         autores.forEach ( autor => {
-          let result = md_template_autor ( autor );
-          fs.writeFileSync( autor.filename, result );
+          console.log ( '\x1b[36m%s\x1b[0m', "+ Autor: " + 'autores/' + autor.filename );
+          console.log ( '\x1b[36m%s\x1b[0m', "+ Mapas: " + JSON.stringify ( autor.mapas ) );
+          let result = md_template_autor ( {name: autor.name, filename: 'autores/' + autor.name + '/', mapas: autor.mapas, mapas_JSON: JSON.stringify(autor.mapas) } );
+          fs.writeFileSync( destdir_autores + '/' + autor.filename + '.md', result );
         });
     }
   });
